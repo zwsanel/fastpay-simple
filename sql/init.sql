5fba4e1c3b67940fea6b6e2239dfb0397f6dc2e4
duanzhongxing-jk <duanzhongxing-jk@360jinrong.net>
2023-04-17 06:08:56 +0800

    微信支付

--- sql/init.sql
+++ sql/init.sql
@@ -8,8 +8,8 @@ create table alipay_app
     out_trade_no    varchar(64)    default ''                not null comment '商户订单号',
     subject         varchar(64)    default ''                not null comment '订单标题',
     total_amount    decimal(17, 2) default 0.00              not null comment '订单总金额，单位为元，精确到小数点后两位',
-    timeout_express varchar(64) null comment '该笔订单允许的最晚付款时间，逾期将关闭交易，超时关闭交易无法继续付款',
-    time_expire     varchar(64) null comment '绝对超时时间，格式为yyyy-MM-dd HH:mm',
+    timeout_express varchar(64)                              null comment '该笔订单允许的最晚付款时间，逾期将关闭交易，超时关闭交易无法继续付款',
+    time_expire     varchar(64)                              null comment '绝对超时时间，格式为yyyy-MM-dd HH:mm',
     status          varchar(64)    default ''                not null comment '订单状态',
     trade_no        varchar(64)    default ''                not null comment '支付宝交易号',
     buyer_logon_id  varchar(100)   default ''                not null comment '买家支付宝账号',
@@ -19,7 +19,7 @@ create table alipay_app
     created_by      varchar(100)   default 'sys'             not null comment '创建人',
     date_updated    timestamp      default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
     updated_by      varchar(100)   default 'sys'             not null comment '修改人',
-    body            varchar(128) null comment '订单描述',
+    body            varchar(128)                             null comment '订单描述',
     constraint uk_request_no
         unique (request_no)
 );
@@ -33,8 +33,6 @@ create index uk_out_trade_no
 create index uk_trade_no
     on alipay_app (trade_no);
 
-
--- auto-generated definition
 create table wechat
 (
     id               bigint unsigned default '0'               not null comment '物理主键'
@@ -94,4 +92,38 @@ create index idx_status
 
 create index idx_transaction_id
     on wechat (transaction_id);
+
+create table wechat_refund
+(
+    id                 int unsigned auto_increment comment '主键'
+        primary key,
+    request_no         varchar(64)  default ''                not null comment '流水号',
+    process_no         varchar(64)  default ''                not null comment '处理号',
+    target_request_no  varchar(64)  default ''                not null comment '原始订单流水',
+    wechat_member_no   varchar(20)  default ''                not null comment '微信商户号',
+    out_refund_no      varchar(64)  default ''                not null comment '退款单号',
+    refund_fee         decimal(17, 2)                         null comment '退款金额',
+    refund_desc        varchar(80)                            null comment '退款原因',
+    return_code        varchar(16)                            null comment '返回状态码',
+    return_msg         varchar(128)                           null comment '返回信息',
+    result_code        varchar(20)  default ''                not null comment '返回码',
+    err_code           varchar(32)                            null comment '错误代码',
+    err_code_des       varchar(128)                           null comment '错误代码描述',
+    refund_id          varchar(32)  default ''                not null comment '微信退款单号',
+    refund_status      varchar(16)  default ''                not null comment '微信退款状态',
+    status             varchar(20)  default ''                not null comment '退款状态',
+    refund_recv_accout varchar(64)  default ''                not null comment '退款入账账户',
+    date_created       timestamp    default CURRENT_TIMESTAMP not null comment '创建时间',
+    created_by         varchar(100) default 'sys'             not null comment '创建人',
+    date_updated       timestamp    default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    updated_by         varchar(100) default 'sys'             not null comment '修改人',
+    constraint uk_process_no
+        unique (process_no),
+    constraint uk_request_no
+        unique (request_no)
+)
+    comment '微信退款';
+
+create index idx_target_request_no
+    on wechat_refund (target_request_no);
 
