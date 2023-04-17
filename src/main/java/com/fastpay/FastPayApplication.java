73bfd8a712e8eac04be919bb0a5e991e5ee0ec27
zero <duanzhongxing-jk@360jinrong.net>
2023-04-18 02:55:16 +0800

    readme

--- src/main/java/com/fastpay/FastPayApplication.java
+++ src/main/java/com/fastpay/FastPayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LilipayApplication {
+public class FastPayApplication {
 
     public static void main( String[] args ) {
-        SpringApplication.run( LilipayApplication.class, args );
+        SpringApplication.run( FastPayApplication.class, args );
     }
 
 }
--- src/test/java/com/fastpay/FastPayApplicationTests.java
+++ src/test/java/com/fastpay/FastPayApplicationTests.java
@@ -10,7 +10,7 @@ import javax.annotation.Resource;
 import java.math.BigDecimal;
 
 @SpringBootTest
-class LilipayApplicationTests {
+class FastPayApplicationTests {
 
 	@Resource
 	private AlipayService alipayService;
